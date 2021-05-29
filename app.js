7d1a1d991e5ecdbd384f0968b007cada8b795147
totalgeek <jranonymous517@gmail.com>
2021-05-29 03:14:20 +0200

    Fix naming and add JavaScript

---
+++ app.js
@@ -0,0 +1,14 @@
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+const url = 'https://api.quotable.io/random';
+const generateQuote = document.getElementById('generate-quote');
+generateQuote.addEventListener('click',()=>{
+  fetch(url)
+  .then(quote => quote.json())
+  .then(data => {
+      console.log(data);
+      quote.innerText = data.content;
+      author.innerText = '- ' + data.author;
+  })
+  .catch(error => alert(error));  
+})
